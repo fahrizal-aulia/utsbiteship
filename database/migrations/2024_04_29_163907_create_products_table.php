e09cfbfb78d146829703a02e0569e4a439b38a7b
fahrizal-aulia <74658393+fahrizal-aulia@users.noreply.github.com>
2024-04-30 00:04:48 +0700

    UTS awal pembuatan migrasi

---
+++ database/migrations/2024_04_29_163907_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->char('sku', 255);
+            $table->unsignedBigInteger('product_category');
+            $table->string('prodct_name');
+            $table->string('prodct_detail');
+            $table->unsignedBigInteger('product_brand');
+            $table->integer('product_price');
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
